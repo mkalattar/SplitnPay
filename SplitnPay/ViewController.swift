1bd0453f5232e0151f883cef7f2d58088134e243
Mohamed Attar <mohamedkattar0@gmail.com>
2020-07-22 23:51:00 -0700

    Initial Commit

---
+++ SplitnPay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitnPay
+//
+//  Created by Mohamed Attar on 7/22/20.
+//  Copyright © 2020 Mohamed Attar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
